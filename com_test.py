552126b24d708aa7ee9dea1cd81e1c2396da82b7
TKWAY <772573480@qq.com>
2021-10-08 20:42:55 +0800

    Update com_test.py

--- com_test.py
+++ com_test.py
@@ -1,23 +1,13 @@
 import serial
-import easygui
 import time
  
-ser = serial.Serial()
-ser.baudrate = 9600
-ser.port = 'COM5'
+ser = serial.Serial()   #这里可以直接初始化
+ser.baudrate = 115200   #波特率
+ser.port = 'COM5'       #串口号
 print(ser)
 ser.open()
-print(ser.is_open)
-i=1
+print(ser.is_open)      #成功打开会显示True
 
 while(1):
-    demo=b"1"
-    ser.write(demo)
-    s = ser.read(1)
-    print(s)
-    time.sleep(0.1)
-    demo = b"2"
-    ser.write(demo)
-    s = ser.read(1)
-    print(s)
-    time.sleep(0.1)
+    data = ser.read(1)  #读取 Byte
+    print(data[1])
