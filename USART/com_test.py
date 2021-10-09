6b5c04b4d85d724be38ad5ee2b75fb944aec08fa
TKWAY <772573480@qq.com>
2021-10-09 13:13:56 +0800

    提取float
    整理文件

--- USART/com_test.py
+++ USART/com_test.py
@@ -1,17 +1,20 @@
 import serial
+import re
  
-ser = serial.Serial()   #这里可以直接初始化
+ser = serial.Serial()
 ser.baudrate = 115200   #波特率
 ser.port = 'COM5'       #串口号
-print(ser)
+print(ser)              #打印串口信息
 ser.open()
-print(ser.is_open)      #成功打开会显示True
-
-Str_flag = 0               #字符串转数据标志位
-i = 0                   
+if(ser.is_open):
+    print("串口打开成功") 
 
 while(1):
     Bytes = ser.readline()              #读取字符串至\n或\r(注意timeout)
     Bytes_Str = Bytes.decode('utf-8')   #byte转字符
     print(Bytes_Str , end='\0')         #字符串中已包含\n，故pirnt不换行
-            
+    Bytes_float = re.findall(r"\d+\.?\d*",Bytes_Str)    #正则转换为
+    PWM = float(Bytes_float[0])
+    
+    print("PWM=" , PWM , type(PWM) , '\n')
+    
